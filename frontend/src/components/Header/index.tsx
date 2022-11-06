6419d4db7a10bb970d31bf6972cb52dd5815fbd3
GabrielaMarangoni <marangonigabriela@gmail.com>
2022-11-06 03:57:09 -0300

    feat: box of places

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import { Button } from "../../components/Button";
 export default function Header() {
 
   return (
-    <div className="absolute w-full bg-cover bg-header p-5 flex justify-between">     
+    <div className="fixed w-full bg-cover bg-header p-5 flex justify-between">     
       <div className="h-28 w-28 bg-cover bg-teste "/>        
     </div>
   )
--- frontend/src/pages/PesquisarLocais/index.tsx
+++ frontend/src/pages/PesquisarLocais/index.tsx
@@ -1,10 +1,7 @@
 import 'remixicon/fonts/remixicon.css';
 import React, { useCallback, useState } from "react"
 import router from 'next/router';
-import { Button } from "../../components/Button";
-import { Input } from "../../components/Input"
-import { Select } from "../../components/Select"
-import { divBack, divGeneral, divImage, divInput, divRegister, textTitle } from "./styles"
+import { divGeneral } from "./styles"
 import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
 
@@ -14,10 +11,89 @@ export default function Search() {
     <div>
       <Header/>
       <Sidebar/>
-      <div className={divGeneral}>
-        <div >
-         
+      <div className={divGeneral}>    
+
+        <div className='w-72 h-80 m-auto mt-5 flex rounded-lg align-itens-center flex-col ' >
+          <h1 className='text-3xl	text-textTitle font-bold'>Nome do lugar</h1>
+          <h2 className='text-2xl text-textTitle pb-4'>Cidade - UF</h2>
+        
+          <div className='h-full bg-black' >
+            oioi
+          </div>
         </div>
+
+        <div className='w-72 h-80 m-auto mt-5 flex rounded-lg align-itens-center flex-col ' >
+          <h1 className='text-3xl	text-textTitle font-bold'>Nome do lugar</h1>
+          <h2 className='text-2xl text-textTitle pb-4'>Cidade - UF</h2>
+        
+          <div className='h-full bg-black' >
+            oioi
+          </div>
+        </div>
+
+        <div className='w-72 h-80 m-auto mt-5 flex rounded-lg align-itens-center flex-col ' >
+          <h1 className='text-3xl	text-textTitle font-bold'>Nome do lugar</h1>
+          <h2 className='text-2xl text-textTitle pb-4'>Cidade - UF</h2>
+        
+          <div className='h-full bg-black' >
+            oioi
+          </div>
+        </div>
+
+        <div className='w-72 h-80 m-auto mt-5 flex rounded-lg align-itens-center flex-col ' >
+          <h1 className='text-3xl	text-textTitle font-bold'>Nome do lugar</h1>
+          <h2 className='text-2xl text-textTitle pb-4'>Cidade - UF</h2>
+        
+          <div className='h-full bg-black' >
+            oioi
+          </div>
+        </div>
+        <div className='w-72 h-80 m-auto mt-5 flex rounded-lg align-itens-center flex-col ' >
+          <h1 className='text-3xl	text-textTitle font-bold'>Nome do lugar</h1>
+          <h2 className='text-2xl text-textTitle pb-4'>Cidade - UF</h2>
+        
+          <div className='h-full bg-black' >
+            oioi
+          </div>
+        </div>
+
+        <div className='w-72 h-80 m-auto mt-5 flex rounded-lg align-itens-center flex-col ' >
+          <h1 className='text-3xl	text-textTitle font-bold'>Nome do lugar</h1>
+          <h2 className='text-2xl text-textTitle pb-4'>Cidade - UF</h2>
+        
+          <div className='h-full bg-black' >
+            oioi
+          </div>
+        </div>
+
+        <div className='w-72 h-80 m-auto mt-5 flex rounded-lg align-itens-center flex-col ' >
+          <h1 className='text-3xl	text-textTitle font-bold'>Nome do lugar</h1>
+          <h2 className='text-2xl text-textTitle pb-4'>Cidade - UF</h2>
+        
+          <div className='h-full bg-black' >
+            oioi
+          </div>
+        </div>
+
+        <div className='w-72 h-80 m-auto mt-5 flex rounded-lg align-itens-center flex-col ' >
+          <h1 className='text-3xl	text-textTitle font-bold'>Nome do lugar</h1>
+          <h2 className='text-2xl text-textTitle pb-4'>Cidade - UF</h2>
+        
+          <div className='h-full bg-black' >
+            oioi
+          </div>
+        </div>
+
+        <div className='w-72 h-80 m-auto mt-5 flex rounded-lg align-itens-center flex-col ' >
+          <h1 className='text-3xl	text-textTitle font-bold'>Nome do lugar</h1>
+          <h2 className='text-2xl text-textTitle pb-4'>Cidade - UF</h2>
+        
+          <div className='h-full bg-black' >
+            oioi
+          </div>
+        </div>
+
+       
         
        
       
