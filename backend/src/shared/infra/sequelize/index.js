a07a73a451c2d99f96bdb455ef8e966a07b7d598
Gaby <44858638+GabrielaMarangoni@users.noreply.github.com>
2022-12-13 23:45:11 -0300

    Update index.js

--- backend/src/shared/infra/sequelize/index.js
+++ backend/src/shared/infra/sequelize/index.js
@@ -2,7 +2,6 @@ const Sequelize = require('sequelize');
 const config = require('./config.json');
 const Places = require('../../../modules/places/infra/sequelize/entities/places');
 const Addresses = require('../../../modules/places/infra/sequelize/entities/addresses');
-const Tags = require('../../../modules/places/infra/sequelize/entities/tags');
 const Images = require('../../../modules/places/infra/sequelize/entities/images');
 const Commentary = require('../../../modules/places/infra/sequelize/entities/commentary');
 
@@ -10,13 +9,11 @@ const connection = new Sequelize(config);
 
 Places.init(connection);
 Addresses.init(connection);
-Tags.init(connection);
 Images.init(connection);
 Commentary.init(connection);
 
 Places.associate(connection.models);
 Addresses.associate(connection.models);
-Tags.associate(connection.models);
 Images.associate(connection.models);
 Commentary.associate(connection.models);
 
